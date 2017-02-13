ff52c21749318c600b0ccdbf7ccb95bb28a48415
kiranhsgithub <hskiran.bms@gmail.com>
2017-02-13 02:34:05 -0500

    variables

--- Nodejs_Essential_Exercise_Files/Ch03/03_01/start/global.js
+++ Nodejs_Essential_Exercise_Files/Ch03/03_01/start/global.js
@@ -3,4 +3,8 @@ var hello = "Hellow World from Node js";
 console.log("Hello, World!");
 console.log("Global Hello ", global.hello);
 console.log("Module Hello ", hello);
-global.console.log("Hello, Global World!")
+global.console.log("Hello, Global World!");
+
+var justNode = hello.slice(17);
+
+console.log(`Rock on World from ${justNode}`);
